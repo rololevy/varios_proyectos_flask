a6a7ab296c8b8bb922a06a3d357b49c22139fd53
rolo-levi <o.oaguilera@gmail.com>
2024-03-14 06:48:41 -0300

    variables de entorno agregada

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+app.config["SECRET_KEY"] = "CLAVE SEGURA"
 
 
 @app.route("/index")
 def index():
     user_ip_information = request.remote_addr
     response = make_response(redirect("/show_information_address"))
-    response.set_cookie("user_ip_information", user_ip_information)
+    session["user_ip_information"] = user_ip_information
     return response
 
 
@@ -28,7 +29,7 @@ def not_found_endpoint(error):
 
 @app.route("/show_information_address")
 def show_information():
-    user_ip = request.cookies.get("user_ip_information")
+    user_ip = session.get("user_ip_information")
     context = {"user_ip": user_ip, "items": items}
     return render_template("ip_information.html", **context)
 
